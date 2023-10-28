581b39f1144f01862f0f53545bf44d516c5f8505
Jouni Linden <jouni.linden@gmail.com>
2023-10-28 12:46:56 +0300

    fixed protocol

--- frontend/src/components/service/http.tsx
+++ frontend/src/components/service/http.tsx
@@ -1,10 +1,10 @@
 
 const protocol = window.location.protocol;
 const domain = window.location.hostname;
-const port = window.location.port;
+const port = window.location.port ? ':' + window.location.port : '';
 
 export const Post = (endpoint: string, body: string): Promise<Response> => {
-    let url = `${protocol}://${domain}:${port}/api/${endpoint}`;
+    let url = `${protocol}//${domain}${port}/api/${endpoint}`;
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -20,7 +20,7 @@ export const Post = (endpoint: string, body: string): Promise<Response> => {
 }
 
 export const Get = (endpoint: string): Promise<Object> => {
-    let url = `${protocol}://${domain}:${port}/api/${endpoint}`;
+    let url = `${protocol}//${domain}${port}/api/${endpoint}`;
     const requestOptions = {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' }
@@ -35,7 +35,7 @@ export const Get = (endpoint: string): Promise<Object> => {
 }
 
 export const Delete = (endpoint: string, id: number): Promise<Object> => {
-    let url = `${protocol}://${domain}:${port}/api/${endpoint}/${id}`;
+    let url = `${protocol}//${domain}${port}/api/${endpoint}/${id}`;
     const requestOptions = {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' }
