d8ac422f080006988aae25573e0f70f5ccd00a71
Jouni Linden <jouni.linden@gmail.com>
2023-10-28 20:43:01 +0300

    omit empty for few json fields

--- app/domain/content.go
+++ app/domain/content.go
@@ -1,8 +1,8 @@
 package domain
 
 type Content struct {
-	ID      int64  `json:"id"`
-	SpaceID int64  `json:"spaceID"`
+	ID      int64  `json:"id,omitempty"`
+	SpaceID int64  `json:"spaceID,omitempty"`
 	Name    string `json:"name"`
 	Value   string `json:"value"`
 }
